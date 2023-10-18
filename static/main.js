a9f393314ed05d4284ca03036702743da62e4522
Linus Stuhlmann <stuhllin@students.zhaw.ch>
2023-10-18 17:03:16 +0200

    update routenplaner

--- static/main.js
+++ static/main.js
@@ -1,6 +1,7 @@
 import { map } from './mapInitialization.js';
 import { locateUser, getCurrentMarker } from './userLocation.js';
 import { updateMap } from './getAmenities.js';
+import { initializeRouting } from './routing.js';
 
 // Event-Listener für den "Benutzer lokalisieren"-Button
 document.getElementById('locate-btn').addEventListener('click', locateUser);
@@ -10,7 +11,14 @@ document.querySelectorAll('input[name="amenity"]').forEach(checkbox => {
     checkbox.addEventListener('change', updateMap);
 });
 
+// Routing
+document.getElementById('navigate-btn').addEventListener('click', function() {
+    initializeRouting(map);
+});
+
 // Benutzer lokalisieren beim Laden des Skripts
 if (!getCurrentMarker()) {
     locateUser();
 }
+
+
---
+++ static/routing.js
@@ -0,0 +1,44 @@
+let routingControl;
+
+export function initializeRouting(map) {
+    console.log("initializeRouting wurde aufgerufen");
+    // Stellen Sie sicher, dass die alte Routing-Kontrolle entfernt wird, bevor eine neue hinzugefügt wird
+    if (routingControl) {
+        map.removeControl(routingControl);
+    }
+
+    const address = document.getElementById('address-input').value;
+    console.log("Eingegebene Adresse:", address);
+
+    const geocoder = L.Control.Geocoder.nominatim();
+    geocoder.geocode(address, function(results) {
+        if (results && results.length > 0) {
+            const latlng = results[0].center;
+
+            // Routing-Optionen
+            const routingOptions = {
+                profile: 'bike', // Verwenden Sie das Profil "bike" für Fahrradrouten
+                avoid: ['motorway'], // Vermeiden Sie Autobahnen
+                useRoads: true, // Erlauben Sie die Verwendung von Straßen
+                usePaths: true, // Erlauben Sie die Verwendung von Radwegen
+                useTraffic: true, //  die Verwendung von Verkehrsdaten
+                useBridges: true, // Erlauben Sie die Verwendung von Brücken
+                useFerries: true, // Erlauben Sie die Verwendung von Fähren
+            };
+
+            routingControl = L.Routing.control({
+                waypoints: [
+                    L.latLng(map.getCenter()), // Startpunkt (momentane Zentrum der Karte)
+                    L.latLng(latlng.lat, latlng.lng) // Ziel (konvertierte Adresse)
+                ],
+                routeWhileDragging: true,
+                router: new L.Routing.OSRMv1({
+                    serviceUrl: 'https://router.project-osrm.org/route/v1',
+                }),
+                ...routingOptions, // Routing-Optionen hinzufügen
+            }).addTo(map);
+        } else {
+            alert("Adresse nicht gefunden!");
+        }
+    });
+}
