b2a1e0bc8c37b17619ed8d8974d561469f07c9d5
Lüthi Isabelle (luethis1) <luethis1@students.zhaw.ch>
2023-11-02 15:49:10 +0100

    button farbe und grösse

--- code/static/js/mapMenuButton.js
+++ code/static/js/mapMenuButton.js
@@ -4,9 +4,9 @@ const checkboxMenu = document.getElementById("checkboxMenu");
 toggleButton.addEventListener("click", () => {
   if (checkboxMenu.classList.contains("hidden")) {
     checkboxMenu.classList.remove("hidden");
-    toggleButton.innerHTML = '<img src="../static/images/menu.png" alt="Menu" />'; // Setze das Bild im Button
+    toggleButton.innerHTML = '<i class=\'bx bx-menu icon\'></i>'; // Setze das Bild im Button
   } else {
     checkboxMenu.classList.add("hidden");
-    toggleButton.innerHTML = '<img src="../static/images/menu.png" alt="Menu" />'; // Setze das Bild im Button
+    toggleButton.innerHTML = '<i class=\'bx bx-menu icon\'></i>'; // Setze das Bild im Button
   }
 });
