a34a023116d2213d48ad26554b71f21d2d2a4477
Linus Stuhlmann <stuhllin@students.zhaw.ch>
2023-11-22 11:02:43 +0100

    skalierung der farbkodierung
    wurzelskaliert

--- code/static/js/chartJs/mapChartJs.js
+++ code/static/js/chartJs/mapChartJs.js
@@ -1,52 +1,83 @@
 function loadFlaskServerData(amenityType, callback) {
-                fetch('/amenities_per_canton', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ amenity_type: amenityType }),
-                })
-                    .then(response => response.json())
-                    .then(data => callback(data))
-                    .catch(error => console.error('Error fetching Flask Server data:', error));
+    // Daten vom Flask Server abrufen
+    fetch('/amenities_per_canton', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ amenity_type: amenityType }),
+    })
+        .then(response => response.json())
+        .then(data => callback(data))
+        .catch(error => console.error('Fehler beim Abrufen der Daten vom Flask Server:', error));
 }
 
-// Hinzufügen einer capitalize-Funktion zum Formatieren von Amenities für das Diagrammlabel
+// Hinzufügen einer capitalize-Funktion zum Formatieren von Annehmlichkeiten für das Diagrammetikett
 String.prototype.capitalize = function () {
     return this.charAt(0).toUpperCase() + this.slice(1);
 };
 
-let chart; // Declare chart variable outside the function to keep a reference to the chart instance
+// TopoJSON-Daten zwischenspeichern, um die Karte schneller zu laden
+let cachedSwitzerlandData = null;
 
+function getSwitzerlandData(callback) {
+    // Überprüfen, ob die Daten bereits zwischengespeichert sind
+    if (cachedSwitzerlandData) {
+        callback(cachedSwitzerlandData);
+    } else {
+        // Daten abrufen und zwischenspeichern
+        fetch('https://swiss-maps.interactivethings.io/api/v0?shapes=cantons&format=topojson')
+            .then((r) => r.json())
+            .then((switzerland) => {
+                cachedSwitzerlandData = switzerland;
+                callback(switzerland);
+            });
+    }
+}
+
+// Deklarieren einer globalen Variablen für die Karte, um sie später zu zerstören
+let chart;
+
+//
 function renderMapWithData(amenityData) {
-    fetch('https://swiss-maps.interactivethings.io/api/v0?shapes=cantons&format=topojson')
-    .then((r) => r.json())
-    .then((switzerland) => {
+    getSwitzerlandData((switzerland) => {
         const cantons = ChartGeo.topojson.feature(switzerland, switzerland.objects.cantons).features;
 
-        // Convert the data into a format suitable for the map
+        // Konvertieren der Rohdaten in das richtige Format für die Karte
         const dataForMap = cantons.map(canton => {
             const cantonId = canton.properties.name;
-            const value = amenityData[cantonId] || 0; // Use 0 if no data is available
+            const value = amenityData[cantonId] || 0;
             return { feature: canton, value: value };
         });
 
+        // Erstellen einer wurzelskalierten Farbskala für die Karte
+        const colorScale = d3.scaleSqrt()
+            .domain([1, d3.max(dataForMap, d => d.value)])
+            .range([0, 1])
+            .interpolate(() => d3.interpolateBlues);
+
+
         const ctx = document.getElementById("mapTopoJSON").getContext("2d");
 
-        // If a chart already exists, destroy it
+        // Wenn bereits eine Karte existiert, zerstören Sie sie
         if (chart) {
             chart.destroy();
         }
 
-        // Create a new chart
+        // Erstellen einer neuen Karte
         chart = new Chart(ctx, {
             type: 'choropleth',
             data: {
                 labels: cantons.map((d) => d.properties.name),
                 datasets: [{
-                    label: 'Cantons of Switzerland',
+                    label: 'Kantone der Schweiz',
                     outline: cantons,
-                    data: dataForMap
+                    data: dataForMap,
+                    backgroundColor: function(context) {
+                        const data = context.dataset.data[context.dataIndex];
+                        const value = data ? data.value : 1; // Verwenden Sie 1 als minimalen Wert für die log-Skala
+                        return colorScale(value);
+                    }
                 }]
             },
             options: {
@@ -62,10 +93,17 @@ function renderMapWithData(amenityData) {
                     },
                     color: {
                         axis: 'x',
-                        quantize: 5,
+                        quantize: 10, // Erhöhen Sie die Anzahl der Quantisierungsstufen
                         legend: {
-                            position: 'bottom-right',
-                            align: 'bottom'
+                            position: 'top-right', // Ändern Sie die Position der Legende
+                            align: 'top', // Ändern Sie die Ausrichtung der Legende
+                            ticks: {
+                                // Anpassen der angezeigten Werte auf der Skala
+                                callback: function(value, index, values) {
+                                    // Konvertieren Sie den Wert zurück zur ursprünglichen Skala
+                                    return Math.round(Math.sqrt(value));
+                                }
+                            }
                         },
                     }
                 },
@@ -78,20 +116,20 @@ function updateMap(selectedAmenity) {
     loadFlaskServerData(selectedAmenity, function (data) {
         var amenityCountPerCanton = {};
 
-        // Fülle amenityCountPerCanton mit den Daten aus data
+        // Füllen von amenityCountPerCanton mit den Daten von Flask
         data.forEach(function (entry) {
             amenityCountPerCanton[entry._id] = entry.count;
         });
 
-        // Aktualisiere die Karte mit den neuen Daten
+        // Aktualisieren der Karte mit den neuen Daten
         renderMapWithData(amenityCountPerCanton);
     });
 }
 
-// Initiieren der Karte mit der Standard-Amenität (Drinking Water)
+// Initiieren der Karte mit der Standard-Annehmlichkeit (Trinkwasser)
 updateMap('drinking_water');
 
-// Dropdown-Änderungsereignis hinzufügen, um die Karte bei Auswahl zu aktualisieren
+// Hinzufügen eines Dropdown-Änderungsereignisses, um die Karte bei Auswahl zu aktualisieren
 document.getElementById('amenitySelect').addEventListener('change', function () {
     var selectedAmenity = this.value;
     updateMap(selectedAmenity);
--- code/static/js/getUserlocation.js
+++ code/static/js/getUserlocation.js
@@ -3,7 +3,6 @@ export function getUserLocation() {
         if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(position => {
                 const user_latlng = L.latLng(position.coords.latitude, position.coords.longitude);
-                console.log(user_latlng);
                 resolve(user_latlng);
             }, () => {
                 reject(new Error("Unable to retrieve location"));
