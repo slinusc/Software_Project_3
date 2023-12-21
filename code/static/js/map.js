1479b353aa272803599409659b9a9044d1eb76b4
Linus Stuhlmann <linus.stuhlmann@gmail.com>
2023-12-21 18:59:19 +0100

    Update map.js

--- code/static/js/map.js
+++ code/static/js/map.js
@@ -1,6 +1,14 @@
-import { map, currentTileLayer, initializeMap, placeCurrentUserMarkerOnMap, getCurrentMarker,
-    updateAmenitiesMap, changeColorOfMarker, debounce } from './mapInitialization.js';
-import {navigateToCoordinates, deleteRoute, navigateToAddress, getAddressFromCoords} from './routing.js';
+import {
+    map, currentTileLayer, initializeMap, placeCurrentUserMarkerOnMap, getCurrentMarker,
+    updateAmenitiesMap, changeColorOfMarker, debounce
+} from './mapInitialization.js';
+import {navigateToCoordinates, deleteRoute, navigateToAddress} from './routing.js';
+
+// Initialisierung globaler Variablen für Routing
+let lastRoutingFunction = null;
+let lastEndLocation = null;
+let destinationCoordinates = null;
+
 
 // Initialisieren der Karte
 initializeMap();
@@ -18,47 +26,45 @@ if (!getCurrentMarker()) {
 }
 
 
-// Event-Listener für den "Benutzer lokalisieren"-Button
-document.getElementById('locate-btn').addEventListener('click', function() {
-    placeCurrentUserMarkerOnMap(localStorage.getItem('mapDarkMode'));
-});
-
-
 // Event-Listener für das Menu-Schaltflaeche
 toggleButton.addEventListener("click", () => {
-  if (checkboxMenu.classList.contains("hidden")) {
-    checkboxMenu.classList.remove("hidden");
-  } else {
-    checkboxMenu.classList.add("hidden");
-  }
-  toggleButton.innerHTML = '<i class=\'bx bx-menu icon\'></i>'; // Setze das Bild im Button
+    if (checkboxMenu.classList.contains("hidden")) {
+        checkboxMenu.classList.remove("hidden");
+    } else {
+        checkboxMenu.classList.add("hidden");
+    }
+    toggleButton.innerHTML = '<i class=\'bx bx-menu icon\'></i>'; // Setze das Bild im Button
 });
 
 
 // Event-Listener für jede Checkbox zum Aktualisieren der Karte
 document.querySelectorAll('input[name="amenity"]').forEach(checkbox => {
-    checkbox.addEventListener('change', function() {
+    checkbox.addEventListener('change', function () {
         updateAmenitiesMap(map);
     });
 });
 
 
 // Routing (Event-Listener für das "amenityClicked"-Event)
-window.addEventListener('amenityClicked', function(e) {
+window.addEventListener('amenityClicked', function (e) {
     const startMarker = getCurrentMarker();
     const start_latlon = [startMarker.getLatLng().lat, startMarker.getLatLng().lng];
     const end_latlon = [e.detail.lat, e.detail.lon];
     navigateToCoordinates(map, start_latlon, end_latlon);
     amenityInfo.style.display = "block";
+    lastRoutingFunction = 'navigateToCoordinates';
+    destinationCoordinates = [e.detail.lat, e.detail.lon];
 });
 
 
 // Event Listener für das Suchfeld (Routing Adresse)
 const searchInput = document.getElementById('searchInput');
-const debouncedNavigate = debounce(function() {
+const debouncedNavigate = debounce(function () {
     const endLocation = searchInput.value;
     const startMarker = getCurrentMarker();
     const start_latlon = [startMarker.getLatLng().lat, startMarker.getLatLng().lng];
+    lastRoutingFunction = 'navigateToAddress';
+    lastEndLocation = endLocation;
     if (endLocation) {
         navigateToAddress(map, start_latlon, endLocation);
     }
@@ -67,13 +73,13 @@ searchInput.addEventListener('input', debouncedNavigate);
 
 
 // Event-Listener für den "Route löschen"-Button
-document.getElementById('delete-btn').addEventListener('click', function() {
+document.getElementById('delete-btn').addEventListener('click', function () {
     deleteRoute(map);
 });
 
 
 // Event-listener für Wechsel zwischen hellen und dunklen Kartenmodus
-document.querySelector('.toggle-switch').addEventListener('click', function() {
+document.querySelector('.toggle-switch').addEventListener('click', function () {
     map.removeLayer(currentTileLayer);
 
     // Ändern Sie die ID des TileLayers je nach aktuellem Modus
@@ -90,3 +96,19 @@ document.querySelector('.toggle-switch').addEventListener('click', function() {
     currentTileLayer.addTo(map);
 });
 
+
+// Event-Listener für den "Benutzer lokalisieren"-Button
+document.getElementById('locate-btn').addEventListener('click', function () {
+
+    placeCurrentUserMarkerOnMap(localStorage.getItem('mapDarkMode'));
+    const startMarker = getCurrentMarker();
+    const startLatLon = [startMarker.getLatLng().lat, startMarker.getLatLng().lng];
+
+    // Überprüft, welche Routing-Funktion zuletzt verwendet wurde und rufe sie erneut auf
+    // um die Route zu aktualisieren bei Standortwechsel
+    if (lastRoutingFunction === 'navigateToCoordinates' && destinationCoordinates) {
+        navigateToCoordinates(map, startLatLon, destinationCoordinates);
+    } else if (lastRoutingFunction === 'navigateToAddress' && lastEndLocation) {
+        navigateToAddress(map, startLatLon, lastEndLocation);
+    }
+});
